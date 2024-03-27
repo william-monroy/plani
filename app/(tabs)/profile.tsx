63a8f56e05572c546c859e272da2fc28f1196792
DennissAyala <ayala.140620@e.unavarra.es>
2024-03-27 16:49:13 +0100

    pos ns cosas del usuario :)

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,19 +1,52 @@
 
 
 import React, { useState, useEffect } from "react";
+
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../_infrastructure/firebase";
 import { View, Button, Text, Image, FlatList, StyleSheet } from "react-native";
 import { getAuth, signOut } from "firebase/auth";
+import { useUserStore } from "@/store/user-store";
+import { User } from "@/types/User.type";
 
 const SettingsPage = () => {
-  // Datos del usuario (simulados)
-  const user = {
-    name: "Nombre",
-    email: "nombre@example.com",
-    photoUrl: "https://example.com/profile.jpg",
-    bio: "¡Hola! Soy xxxx, blblblbblablalbalbalblabal."
-  };
+  const { firstName, email } = useUserStore.getState();
+  
+  // // Datos del usuario (simulados)
+  // const user = {
+  //   name: firstName,
+  //   email: email,
+  //   photoUrl: "https://example.com/profile.jpg",
+  //   bio: "¡Hola! Soy xxxx, blblblbblablalbalbalblabal."
+  // };
+
+  const getData = async () => {
+    const collectionRef = collection(db, "Usuarios");
+    
+  const [usuarios, setUsuarios] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+    await onSnapshot(collectionRef, async (data) => {
+      setUsuarios(
+        await data.docs.map((item) => {
+          const userData = { ...item.data(), id: item.id } as unknown;
+          return userData as User;
+        })
+      );
+      console.log("Users updated", JSON.stringify(usuarios, null, 2));
+      setIsLoading(false);
+    });
+
+    useEffect(() => {
+      getData();
+      console.log("re-render");
+    }, []);
 
   return (
+    // {isLoading ? (
+    //   <Text>Loading users...</Text>
+    // ) : (
+    //   {usuarios.map((plan: Plan, key: number) => (
     <View style={styles.container}>
       {/* Foto y nombre del usuario */}
       <View style={styles.userInfo}>
