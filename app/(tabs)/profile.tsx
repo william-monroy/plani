1469e33f829ff8836e329b48f523d502d7dd6daa
Andrea <morala.138201@e.unavarra.es>
2024-04-10 16:53:42 +0200

    feat: 🎸 perfil del usuario muestra planes y foto de perfil

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,69 +1,246 @@
-
-
 import React, { useState, useEffect } from "react";
-
-import { collection, onSnapshot } from "firebase/firestore";
-import { db } from "../_infrastructure/firebase";
-import { View, Button, Text, Image, FlatList, StyleSheet } from "react-native";
+import { Link, router, useLocalSearchParams } from "expo-router";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  query,
+  where,
+  onSnapshot } from "firebase/firestore";
 import { getAuth, signOut } from "firebase/auth";
+import { db, storage } from "../_infrastructure/firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { View, Button, Text, Image, FlatList, StyleSheet, Pressable, TouchableOpacity, Dimensions, ScrollView } from "react-native";
 import { useUserStore } from "@/store/user-store";
 import { User } from "@/types/User.type";
+import * as ImagePicker from "expo-image-picker";
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { Plan } from "@/types/Plan.type";
+import { PlanCard } from "@/components/PlanCard";
 
-const SettingsPage = () => {
-  const { firstName, email } = useUserStore.getState();
-  
-  // // Datos del usuario (simulados)
-  // const user = {
-  //   name: firstName,
-  //   email: email,
-  //   photoUrl: "https://example.com/profile.jpg",
-  //   bio: "¡Hola! Soy xxxx, blblblbblablalbalbalblabal."
-  // };
 
-  const getData = async () => {
-    const collectionRef = collection(db, "Usuarios");
-    
-  const [usuarios, setUsuarios] = useState<User[]>([]);
+
+
+
+
+
+
+export const SettingsPage = () => {
+  // const { firstName, email } = useUserStore.getState();
+  const [user, setUser] = useState<User>({} as User);
+  const [refreshData, setRefreshData] = useState(0); // Añade este estado
+  const [image, setImage] = useState<string>("" as string); // Añade este estado
+  const [index, setIndex] = useState(0);
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [planes, setPlanes] = useState<Plan[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  //desde aqui
+  const [routes] = useState([
+    { key: 'myPlans', title: 'Mis Planes' },
+    { key: 'joinedPlans', title: 'Apuntado' },
+  ]);
+
+  const initialLayout = { width: Dimensions.get('window').width };
+
+  const MyPlansRoute = () => (
+    <View style={[styles.scene, { backgroundColor: '#ffffff' }]}>
+      {/* <Text>Mis Planes</Text> */}
+      <View style={styles.container2_index}>
+        {/* <Text style={[{ marginBottom: 15 }, styles.subTitle_index]}>
+          Planes cercanos
+        </Text> */}
+        {isLoading ? (
+          <Text>Loading users...</Text>
+        ) : (
+          <ScrollView style={styles.plans_index}>
+            {planes.map((plan: Plan, key: number) => (
+              <PlanCard key={key} {...plan} />
+            ))}
+          </ScrollView>
+        )}
+      </View>
+      {/* Aquí puedes añadir la lógica para mostrar los planes publicados por el usuario */}
+    </View>
+  );
+
+  const JoinedPlansRoute = () => (
+    <View style={[styles.scene, { backgroundColor: '#ffffff' }]}>
+      <Text>Planes a los que me he unido</Text>
+      {/* Aquí puedes añadir la lógica para mostrar los planes a los que el usuario se ha unido */}
+    </View>
+  );
+
+  const getData = async () => {
+    const collectionRef = collection(db, "Planes");
+
     await onSnapshot(collectionRef, async (data) => {
-      setUsuarios(
+      setPlanes(
         await data.docs.map((item) => {
-          const userData = { ...item.data(), id: item.id } as unknown;
-          return userData as User;
+          const planData = { ...item.data(), id: item.id } as unknown;
+          return planData as Plan;
         })
       );
-      console.log("Users updated", JSON.stringify(usuarios, null, 2));
+      console.log("Planes updated", JSON.stringify(planes, null, 2));
       setIsLoading(false);
     });
+  };
 
-    useEffect(() => {
-      getData();
-      console.log("re-render");
-    }, []);
+  const renderScene = SceneMap({
+    myPlans: MyPlansRoute,
+    joinedPlans: JoinedPlansRoute,
+  });
+  //hasta aqui
+
+
+  const { uid } = useLocalSearchParams();
+
+  const pickImage = async () => {
+    try {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission denied");
+        return;
+      }
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+        updatePlan();
+      }
+    } catch (error) {
+      console.error("Error picking image: ", error);
+    }
+  };
+
+  const updatePlan = async () => {
+    const userId = user.uid;
+    try {
+      const uri = image as string;
+      const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+          resolve(xhr.response);
+        };
+        xhr.onerror = function (e) {
+          reject(new TypeError("Network request failed"));
+        };
+        xhr.responseType = "blob";
+        xhr.open("GET", uri, true);
+        xhr.send(null);
+      });
+      const filename = uri.substring(uri.lastIndexOf("/") + 1);
+      const storageRef = ref(storage, `planes/${userId}/${filename}`);
+      const uploadTask = uploadBytesResumable(storageRef, blob as Blob);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + Math.round(progress) + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          console.error("Error uploading image: ", error);
+        },
+
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(
+            async (downloadURL) => {
+              console.log("File available at", downloadURL);
+              setImageUrl(downloadURL);
+              const docRef = doc(db, "Usuarios", userId as string);
+                updateDoc(docRef, { avatar: downloadURL })
+                .then(() => {
+                  setRefreshData((prev) => prev + 1);
+                })
+                .catch((error) => {
+                  console.error("Error updating document: ", error);
+                });
+            }
+          );
+        }
+      );
+    } catch (error) {
+      console.error("Error uploading image: ", error);
+    }
+  };
+
+  const getUserData = async () => {
+    const userId = useUserStore.getState().uid;
+    try {
+        let userData : User = {} as User;
+        const docRef = doc(db, "Usuarios", userId as string);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          console.log("User data:", docSnap.data());
+          userData = docSnap.data() as User;
+        }
+        setUser(userData);
+        console.log(user)
+      } catch (error) {
+      console.error("Error getting documents: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    getUserData();
+  }, [uid, refreshData]);
 
   return (
-    // {isLoading ? (
-    //   <Text>Loading users...</Text>
-    // ) : (
-    //   {usuarios.map((plan: Plan, key: number) => (
+
     <View style={styles.container}>
       {/* Foto y nombre del usuario */}
       <View style={styles.userInfo}>
-        <Image source={{ uri: user.photoUrl }} style={styles.userPhoto} />
-        <Text style={styles.userName}>{user.name}</Text>
+        <TouchableOpacity onPress={pickImage}>
+          <Image source={{ uri: user.avatar as string }} style={styles.userPhoto} />
+        </TouchableOpacity>
+        <Text style={styles.userName}>{user.firstName} {user.lastName}</Text>
       </View>
 
-      {/* Correo electrónico */}
-      <Text style={styles.label}>Correo electrónico:</Text>
-      <Text style={styles.info}>{user.email}</Text>
 
-      {/* Biografía */}
-      <Text style={styles.label}>Biografía:</Text>
-      <Text style={styles.info}>{user.bio}</Text>
-    
       {/* Botón de Cerrar sesión */}
-      <Button title="Cerrar sesión" onPress={async () => await signOut(getAuth())} />
+      <View style={styles.container2}>
+        <Pressable style={styles.button} onPress={async () => await signOut(getAuth())}>
+          <Text style={styles.textButton}>Cerrar sesión</Text>
+        </Pressable>
+      </View>
+
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+        renderTabBar={props => (
+          <TabBar
+            {...props}
+            indicatorStyle={styles.indicator}
+            style={styles.tabBar}
+            labelStyle={styles.label2}
+            activeColor="#FF9500" // Naranja para el texto de la pestaña activa
+            inactiveColor="#F3A462" // Un tono más claro de naranja para las pestañas inactivas
+          />
+        )}
+      />
     </View>
   );
 };
@@ -74,10 +251,28 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: "#fff"
   },
+  scene: {
+    flex: 4,
+  },
+  tabBar: {
+    backgroundColor: 'white', // Fondo blanco para un look minimalista
+    shadowOpacity: 0, // Quitamos cualquier sombra para un diseño más limpio
+    elevation: 0, // Eliminamos la elevación en Android
+    borderBottomWidth: 1, // Añadimos un borde sutil en la parte inferior
+    borderBottomColor: '#FF9500', // El color del borde es naranja para mantener tu tema
+  },
+  indicator: {
+    backgroundColor: '#FF9500', // Color del indicador de la pestaña activa en naranja
+    height: 3, // Hacemos el indicador un poco más grueso para que destaque
+  },
+  label: {
+    fontWeight: 'bold', // Texto en negrita para las etiquetas de las pestañas
+    textTransform: 'none', // Mantenemos el caso de texto original sin transformar
+  },
   userInfo: {
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 20
+    //marginBottom: 10
   },
   userPhoto: {
     width: 100,
@@ -89,33 +284,49 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold"
   },
-  label: {
-    fontSize: 18,
+  label2: {
+    fontSize: 16,
     fontWeight: "bold",
     marginTop: 10
   },
   info: {
     fontSize: 16
-  },button: {
-    backgroundColor: "#FF9500", // Cambiado a un naranja más vibrante
-    borderRadius: 60, // Bordes más redondeados para un look moderno
-    height: 40,
-    width: "75%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginTop: "30%",
+  },
+  container2: {
+    marginTop: 20, // Ajustamos el margen superior para separarlo de otros elementos
+    alignItems: "center", // Aseguramos que el botón esté centrado horizontalmente
     marginBottom: 20,
-    shadowColor: "#000", // Sombra para dar profundidad
-    shadowOffset: {
-      width: 0,
-      height: 4, // Ajustamos la altura para que la sombra sea más notable
-    },
-    shadowOpacity: 0.3, // Opacidad de la sombra
-    shadowRadius: 4, // Difuminado de la sombra
-    elevation: 8, // Elevación para Android, aumentada para mayor sombra
-  }
+  },
+  button: {
+    backgroundColor: "transparent", // Fondo transparente para un look más sutil
+    borderWidth: 2, // Grosor del borde
+    borderColor: "#FF9500", // Color naranja para el borde, manteniendo el esquema de color
+    borderRadius: 25, // Bordes redondeados
+    height: 40, // Altura moderada
+    width: 200, // Ancho fijo para controlar el tamaño del botón
+    justifyContent: "center", // Alinea el texto verticalmente
+    alignItems: "center", // Alinea el texto horizontalmente
+    shadowOpacity: 0, // Eliminamos la sombra para mantener el aspecto minimalista
+    elevation: 0, // Eliminamos la elevación en Android
+  },
+  textButton: {
+    color: "#FF9500", // Utilizamos el mismo naranja para el texto, creando una cohesión visual
+    fontWeight: "600", // Peso moderado del texto
+    fontSize: 16, // Tamaño adecuado del texto para legibilidad
+  },
+  container2_index: {
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  plans_index: {
+    display: "flex",
+    gap: 10,
+    height: "100%",
+  },
+  subTitle_index: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
+
 export default SettingsPage;
