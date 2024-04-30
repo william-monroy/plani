29bb11f2820ae311bf00d67458d98b49beaa920a
fzr <zubiri.126123@e.unavarra.es>
2024-04-30 16:46:13 +0200

    feat: 🎸 Implememtacion de solicitudes para planes
    Ahora puedes aceptar o rechazar solicitudes

--- app/(tabs)/solicitudes/[id].tsx
+++ app/(tabs)/solicitudes/[id].tsx
@@ -6,120 +6,258 @@ import { Plan } from "@/types/Plan.type";
 import { Solicitud } from "@/types/Solicitud";
 import { useLocalSearchParams } from "expo-router";
 import { User } from "firebase/auth";
-import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { ScrollView, Text, View, StyleSheet, RefreshControl } from "react-native";
+import {
+  ScrollView,
+  Text,
+  View,
+  StyleSheet,
+  RefreshControl,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-
 export default function SolicitudScreen() {
-    const insets = useSafeAreaInsets();
-    //const planId = id;
-    
-    const [solicitudes, setSolicitudes] = useState<Solicitud[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [solicitudesId, setsolicitudesId] = useState<Solicitud[]>([]);
-    const { id } = useLocalSearchParams();
-    const [refreshing, setRefreshing] = useState(false);
+  const insets = useSafeAreaInsets();
+  //const planId = id;
 
+  const [solicitudes, setSolicitudes] = useState<Solicitud[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [solicitudesId, setsolicitudesId] = useState<Solicitud[]>([]);
+  const { id } = useLocalSearchParams();
+  const [refreshing, setRefreshing] = useState(false);
 
+  const getSolicitudData = async () => {
+    try {
+      const q = query(collection(db, "Planes"), where("uid", "==", id));
+      const querySnapshot = await getDocs(q);
+      const firstDoc = querySnapshot.docs[0];
 
-    
+      if (firstDoc) {
+        // Extraer el campo "solicitud" del documento
+        setsolicitudesId([]);
+        setSolicitudes([]);
+        console.log("solicitudId" + solicitudesId);
+        const solicitud = firstDoc.data().solicitud;
+        console.log("Solicitud:", solicitud);
+        await setsolicitudesId(solicitud);
+        console.log("Aquiiiiiiiiiiii" + solicitudesId);
 
-    const getSolicitudData = async () => {
-        try {
-            const q = query(collection(db, "Planes"), where("uid", "==", id));
-            const querySnapshot = await getDocs(q);
-            const firstDoc = querySnapshot.docs[0];
-            if (firstDoc) {
-                // Extraer el campo "solicitud" del documento
-                const solicitud = firstDoc.data().solicitud;
-                console.log("Solicitud:", solicitud);
-                setSolicitudes([]);
-                setsolicitudesId(solicitud); 
-                getUsersData();
-            } else {
-                console.log("No se encontraron documentos.");
-            }
-        } catch (error) {
-            console.error("Error getting solicitud data: ", error);
-        }
-    };
+        getUsersData();
+      } else {
+        console.log("No se encontraron documentos.");
+      }
+    } catch (error) {
+      console.error("Error getting solicitud data: ", error);
+    }
+  };
 
+  const getUsersData = async () => {
+    console.log("laaaaaaaaa   -", solicitudesId);
 
-    const getUsersData = async () => {
-        console.log("laaaaaaaaa   -",solicitudesId )
-        try {
-            const q = query(collection(db, "Usuarios"), where("uid", "in", solicitudesId));
-            const querySnapshot = await getDocs(q);
-            const userData: Solicitud[] = querySnapshot.docs.map((doc) => ({
-                idUsuario: doc.id,
-                avatar: doc.data().avatar,
-                // Suponiendo que tienes los campos "firstName" y "lastName" en tus documentos de usuario
-                firstName: doc.data().firstName,
-                lastName: doc.data().lastName,
-                planId : id
-            }));
-            console.log("Esto son los usuarios extraidos:  ",userData)
-            setSolicitudes(userData);
-        } catch (error) {
-            console.log("Non User data ", error); 
-        }
-        setIsLoading(false);
-    };
-        // Función para recargar los datos
-        const reloadData = () => {
-            setsolicitudesId([]);
-            setSolicitudes([]);
-            setIsLoading(true);
-            getSolicitudData(); 
-        };
-    
-    const onRefresh = () => {
-        //getData();  // Puedes optar por llamar a getData o cualquier otra función que actualice tus datos
-        setRefreshing(true);
-        getSolicitudData();
-        setRefreshing(false);
-    };
-
-    useEffect(() => {
-        reloadData()
-        console.log(solicitudes)
-        console.log("esteeeeeeee ---- "+ id) 
-    }, [id]); // Eliminar refreshData como dependencia
-
-
-
-    return (
-        <View style={[styles.container, { paddingTop: insets.top }]}>
-            <ScrollView style={styles.solicituds} refreshControl={
-              <RefreshControl
-                refreshing={refreshing}
-                onRefresh={onRefresh}
-                
-              />
-            }> 
-                {isLoading ? (
-                    <Text>Loading users...</Text>
-                )  : solicitudes.length === 0 ? (
-                    <Text>No hay solicitudes</Text>
-                ) : (
-                    solicitudes.map((solicitud, index) => (
-                        <SolicitudCard  onRefreshData={reloadData} key={index} avatar={solicitud.avatar || `https://ui-avatars.com/api/?name=${solicitud.firstName.split(" ")[0]}+${solicitud.lastName.split(" ")[0]}&background=random&color=fff`} idUsuario={solicitud.idUsuario} lastName={solicitud.lastName} firstName={solicitud.firstName} planId = {id}/>
-                    ))
-                )}
-            </ScrollView>
-        </View>
+    const collectionRef = query(
+      collection(db, "Usuarios"),
+      where("uid", "in", solicitudesId)
     );
+
+    await onSnapshot(collectionRef, async (data) => {
+      //console.log("holaaaaaaaaaaaaaaa :"+JSON.stringify(data.docs,null,2));
+      //await data.docs.map((doc) => {
+      //})
+
+      setSolicitudes(
+        (await data.docs.map)
+          ? await data.docs.map((solicitud: any) => {
+              return {
+                idUsuario: solicitud.idUsuario,
+                avatar: solicitud.avatar,
+                // Suponiendo que tienes los campos "firstName" y "lastName" en tus solicitudumentos de usuario
+                firstName: solicitud.firstName,
+                lastName: solicitud.lastName,
+                planId: id,
+              } as Solicitud;
+            })
+          : []
+      );
+    });
+
+    try {
+      const q = query(
+        collection(db, "Usuarios"),
+        where("uid", "in", solicitudesId)
+      );
+      const querySnapshot = await getDocs(q);
+      const userData: Solicitud[] = querySnapshot.docs.map((doc) => ({
+        idUsuario: doc.id,
+        avatar: doc.data().avatar,
+        // Suponiendo que tienes los campos "firstName" y "lastName" en tus documentos de usuario
+        firstName: doc.data().firstName,
+        lastName: doc.data().lastName,
+        planId: id,
+      }));
+      console.log("Esto son los usuarios extraidos:  ", userData);
+      if (userData != solicitudes) {
+        setSolicitudes(userData);
+      }
+    } catch (error) {
+      console.log("Non User data ", error);
+    }
+    setIsLoading(false);
+  };
+  // Función para recargar los datos
+  const reloadData = () => {
+    setIsLoading(true);
+    setsolicitudesId([]);
+    setSolicitudes([]);
+    getSolicitudData();
+  };
+
+  const onRefresh = (x?: any) => {
+    //getData();  // Puedes optar por llamar a getData o cualquier otra función que actualice tus datos
+    setRefreshing(true);
+    console.log("La solicitud que hemos eliminado es : " + x);
+    setsolicitudesId([]);
+    setSolicitudes([]);
+    getSolicitudData();
+    const solicitudEncontrada = solicitudes.find(
+      (solicitud) => solicitud.idUsuario === x
+    );
+
+    if (solicitudEncontrada) {
+      setSolicitudes(
+        solicitudes.filter((solicitud) => solicitud.idUsuario !== x)
+      );
+      console.log("solicitudes a enseñar " + solicitudes);
+    } else {
+      console.log("La solicitud no fue encontrada en el vector.");
+    }
+    setRefreshing(false);
+  };
+
+  useEffect(() => {
+    reloadData();
+    console.log(solicitudes);
+    console.log("esteeeeeeee ---- " + id);
+  }, [id]); // Eliminar refreshData como dependencia
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const q = query(
+          collection(db, "Usuarios"),
+          where("uid", "in", solicitudesId)
+        );
+        const querySnapshot = await getDocs(q);
+        const userData: Solicitud[] = querySnapshot.docs.map((doc) => ({
+          idUsuario: doc.id,
+          avatar: doc.data().avatar,
+          firstName: doc.data().firstName,
+          lastName: doc.data().lastName,
+          planId: id,
+        }));
+        console.log("userData:",userData)
+        setSolicitudes(userData);
+      } catch (error) {
+        console.log("Non User data ", error);
+      }
+      //try {
+      //  const q = query(
+      //    collection(db, "Usuarios"),
+      //    where("uid", "in", solicitudesId)
+      //  );
+      //  const querySnapshot = await getDocs(q);
+      //  const userData: Solicitud[] = querySnapshot.docs.map((doc) => ({
+      //    idUsuario: doc.id,
+      //    avatar: doc.data().avatar,
+      //    // Suponiendo que tienes los campos "firstName" y "lastName" en tus documentos de usuario
+      //    firstName: doc.data().firstName,
+      //    lastName: doc.data().lastName,
+      //    planId: id,
+      //  }));
+      //  console.log("Esto son los usuarios extraidos:  ", userData);
+      //  setSolicitudes(userData);
+      //} catch (error) {
+      //  console.log("Non User data ", error);
+      //}
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [solicitudesId]);
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <ScrollView
+        style={styles.solicituds}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {isLoading ? (
+          <Text>Loading users...</Text>
+        ) : solicitudes.length === 0 ? (
+          <Text>No hay solicitudes</Text>
+        ) : (
+          solicitudes.map((solicitud, index) => (
+            <SolicitudCard
+              onRefreshData={reloadData}
+              key={index}
+              avatar={
+                solicitud.avatar ||
+                `https://ui-avatars.com/api/?name=${
+                  solicitud.firstName?.split(" ")[0]
+                }+${
+                  solicitud.lastName?.split(" ")[0]
+                }&background=random&color=fff`
+              }
+              idUsuario={solicitud.idUsuario}
+              lastName={solicitud.lastName}
+              firstName={solicitud.firstName}
+              planId={id}
+            />
+          ))
+        )}
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#f9f9f9",
-    },
-    solicituds: {
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#f9f9f9",
+  },
+  solicituds: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
 });
+function onDelete(id: any) {
+  throw new Error("Function not implemented.");
+}
+
+function async(data: any): {
+  next?:
+    | ((
+        snapshot: import("@firebase/firestore").QuerySnapshot<
+          import("@firebase/firestore").DocumentData,
+          import("@firebase/firestore").DocumentData
+        >
+      ) => void)
+    | undefined;
+  error?:
+    | ((error: import("@firebase/firestore").FirestoreError) => void)
+    | undefined;
+  complete?: (() => void) | undefined;
+} {
+  throw new Error("Function not implemented.");
+}
--- components/SolicitudCard.tsx
+++ components/SolicitudCard.tsx
@@ -1,152 +1,179 @@
 import { db } from "@/app/_infrastructure/firebase";
 import { Solicitud } from "@/types/Solicitud";
 import { User } from "@/types/User.type";
-import { collection, doc, getDoc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import { router, useNavigation } from "expo-router";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-
-
 export const SolicitudCard = (props: Solicitud) => {
-    const [refreshCount, setRefreshCount] = useState(0);
+  const [refreshCount, setRefreshCount] = useState(0);
+  const [aux, setAux] = useState<String>();
 
+  const navigation = useNavigation()
 
-    interface Solicitud  {
-        // Otras propiedades de Solicitud
-        guests: string[]; 
-        solicitud: string[];
-        onRefreshData: () => void;
+  interface Solicitud {
+    // Otras propiedades de Solicitud
+    guests: string[];
+    solicitud: string[];
+    onRefreshData: () => void;
+  }
+
+  const aceptarSol = async () => {
+    console.log("eliminamos de solicitud del plan", props.idUsuario);
+    setAux(props.idUsuario.toString());
+
+    try {
+      const docRef = doc(db, "Planes", props.planId.toString());
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        // Obtener los datos del documento
+        const docData = docSnap.data() as Solicitud;
+
+        // Obtener la lista actual de invitados o crear una nueva si no existe
+        const guests = docData.guests || [];
+
+        // Crear un nuevo objeto de datos con el usuario añadido a la lista de invitados
+        const newData: Partial<Solicitud> = {
+          ...docData, // Incluir todas las propiedades existentes
+          guests: [...guests, props.idUsuario.toString()], // Agregar userId al campo guests
+          solicitud: docData.solicitud.filter(
+            (id: string) => id !== props.idUsuario
+          ), // Eliminar userId de la lista de solicitudes
+        };
+
+        // Actualizar el documento con el nuevo objeto de datos
+        await updateDoc(docRef, newData);
+
+        console.log(
+          "Usuario movido de la lista de solicitudes a la lista de invitados en Firestore"
+        );
+      } else {
+        console.log("El documento no existe en Firestore");
+      }
+    } catch (error) {
+      console.error(
+        "Error al mover el usuario de la lista de solicitudes a la lista de invitados:",
+        error
+      );
+      throw error;
+    } 
+  };
+
+  const eliminarSol = async () => {
+    console.log("eliminamos de solicitud del plan", props.idUsuario);
+    setAux(props.idUsuario);
+    console.log("El puto AUX : ", aux);
+
+    try {
+      // Obtener el documento
+
+      const docRef = doc(db, "Planes", props.planId.toString());
+      const docSnap = await getDoc(docRef);
+      // Verificar si el documento existe
+
+      if (docSnap.exists()) {
+        // Obtener el vector del documento
+        const datos = docSnap.data();
+
+        console.log("Aux : " + aux);
+        const nuevoVector = datos.solicitud.filter(
+          (elem: String) => elem !== props.idUsuario
+        );
+        console.log("idUsuario: " + props.idUsuario);
+
+        // Actualizar el documento con el nuevo vector
+        await updateDoc(docRef, { solicitud: nuevoVector });
+
+        console.log("Elemento eliminado correctamente");
+      } else {
+        console.log("El documento no existe");
+      }
+    } catch (error) {
+      console.error("Error al eliminar elemento:", error);
     }
-   
-    const aceptarSol = async () => {
-        console.log("eliminamos de solicitud del plan",props.idUsuario);
-    
-        try {
-            const docRef = doc(db, 'Planes', props.planId.toString());
-            const docSnap = await getDoc(docRef);
-            
-            if (docSnap.exists()) {
-                // Obtener los datos del documento
-                const docData = docSnap.data() as Solicitud;
-    
-                // Obtener la lista actual de invitados o crear una nueva si no existe
-                const guests = docData.guests || [];
-    
-                // Crear un nuevo objeto de datos con el usuario añadido a la lista de invitados
-                const newData: Partial<Solicitud> = {
-                    ...docData, // Incluir todas las propiedades existentes
-                    guests: [...guests, props.idUsuario.toString()], // Agregar userId al campo guests
-                    solicitud: docData.solicitud.filter((id: string) => id !== props.idUsuario) // Eliminar userId de la lista de solicitudes
-                };
-    
-                // Actualizar el documento con el nuevo objeto de datos
-                await updateDoc(docRef, newData);
-                
-                console.log('Usuario movido de la lista de solicitudes a la lista de invitados en Firestore');
-            } else {
-                console.log('El documento no existe en Firestore');
-            }
-            
-        } catch (error) {
-            console.error('Error al mover el usuario de la lista de solicitudes a la lista de invitados:', error);
-            throw error;
-        }
-    };
-    
+  };
 
-    
-    const eliminarSol = async () => {
-        console.log("eliminamos de solicitud del plan",props.idUsuario)
+  useEffect(() => {
+    console.log("SolicitudCard props:", props);
+    console.log("Aux actualizado:", aux);
+  }, [refreshCount, aux]);
 
-        try {
-            // Obtener el documento
+  function onRefreshData() {
+    throw new Error("Function not implemented.");
+  }
 
-            const docRef = doc(db, "Planes", props.planId.toString());
-            const docSnap = await getDoc(docRef);
-            // Verificar si el documento existe
-            
-            if (docSnap.exists()) {
-                // Obtener el vector del documento
-                const datos = docSnap.data();
-                const nuevoVector = datos.solicitud.filter((elem: String) => elem !== props.idUsuario);
-                console.log(props.idUsuario)
-    
-                // Actualizar el documento con el nuevo vector
-                await updateDoc(docRef, { solicitud: nuevoVector  });
-    
-                console.log("Elemento eliminado correctamente");
-            } else {
-                console.log("El documento no existe");
-            }
-            
-        } catch (error) {
-            console.error("Error al eliminar elemento:", error);
-        }
-      };
+  return (
+    <View style={styles.container}>
+      {/* Foto a la izquierda */}
+      <Image source={{ uri: props.avatar.toString() }} style={styles.image} />
 
-    useEffect(() => {
-        console.log("SolicitudCard props:", props);
-        
-    }, [refreshCount]);
+      {/* Botón verde en el medio */}
+      <TouchableOpacity
+        style={[styles.button, styles.greenButton]}
+        onPress={() => {
+          aceptarSol();
+          props.onRefreshData(props.idUsuario);
+          
+        }}
+      >
+        <Text style={styles.buttonText}>Aceptar </Text>
+      </TouchableOpacity>
 
-    function onRefreshData() {
-        throw new Error("Function not implemented.");
-    }
-
-    return (
-        <View style={styles.container}>
-            {/* Foto a la izquierda */}
-            <Image source={{ uri: props.avatar.toString() }} style={styles.image}/>
-        
-            {/* Botón verde en el medio */}
-            <TouchableOpacity style={[styles.button, styles.greenButton]} onPress={() => {
-                                                                                    aceptarSol();
-                                                                                    props.onRefreshData();
-                                                                                }}>
-                <Text style={styles.buttonText}>Aceptar </Text>
-            </TouchableOpacity>
-        
-            {/* Botón rojo a la derecha */}
-            <TouchableOpacity style={[styles.button, styles.redButton]} onPress={() => {
-                                                                                    eliminarSol();
-                                                                                    props.onRefreshData();
-                                                                                }}>
-                <Text style={styles.buttonText}>Rechazar</Text>
-            </TouchableOpacity>
-        </View>
-    );
+      {/* Botón rojo a la derecha */}
+      <TouchableOpacity
+        style={[styles.button, styles.redButton]}
+        onPress={() => {
+          eliminarSol();
+          console.log("Esto es props.idUsuario : " + props.idUsuario);
+          props.onRefreshData(props.idUsuario);
+        }}
+      >
+        <Text style={styles.buttonText}>Rechazar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        backgroundColor: "#ffffff",
-        borderRadius: 10,
-        elevation: 5,
-    },
-    image: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-    },
-    button: {
-        padding: 10,
-        borderRadius: 5,
-        elevation: 2,
-    },
-    greenButton: {
-        backgroundColor: "green",
-    },
-    redButton: {
-        backgroundColor: "red",
-    },
-    buttonText: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center",
-    },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+    elevation: 5,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    elevation: 2,
+  },
+  greenButton: {
+    backgroundColor: "green",
+  },
+  redButton: {
+    backgroundColor: "red",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
